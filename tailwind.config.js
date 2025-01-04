1aa616d8afaff2d36feed5698800d3fed5245e67
Nikoloz Gigiashvili <gigiashvilinikoloz@gmail.com>
2025-01-04 15:25:15 +0400

    git init ( finish everything 5 hours (need more tools) )

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      '400phone': '400px',
+      '450phone': '450px',
+      '700phone': '700px',
+      '750phone': '750px',
+      '800phone': '800px',
+      '850phone': '850px',
+      '900phone': '900px',
+      '1000phone': '1000px',
+    },
+    extend: {
+      fontFamily: {
+        'fonta': ['Bree Serif', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
